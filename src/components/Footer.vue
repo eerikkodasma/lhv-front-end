321ca407567989045dda8e77b87710063e39eeeb
eekoda <eekoda@ttu.ee>
2024-06-11 16:33:40 +0300

    Footer component mobile friendly

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="footer">
+  <div v-if="!isMobile" class="footer">
     <div class="section-wrapper">
       <img src="../assets/images/support.png" alt="headset" />
       <section class="section">
-        <h2>LHV <span>Klienditugi</span></h2>
+        <h2 class="footer-title">
+          LHV <span class="footer-title-bold">Klienditugi</span>
+        </h2>
         <p>
           Kui sul tekib pangateenuse kasutamisel probleeme, saad klienditoe
           infotelefonilt abi ööpäev ringi.
@@ -15,13 +17,36 @@
       <p><img src="../assets/icons/letter.svg" alt="letter" />info@lhv.ee</p>
     </div>
   </div>
+  <div v-else class="footer-mobile">
+    <div class="footer-mobile-wrapper">
+      <img src="../assets/images/support.png" alt="headset" />
+      <h2 class="footer-title">
+        LHV <span class="footer-title-bold">Klienditugi</span>
+      </h2>
+      <div class="contact-mobile-wrapper">
+        <h2><img src="../assets/icons/phone.svg" alt="phone" />6 800 400</h2>
+        <h2>
+          <img src="../assets/icons/letter.svg" alt="letter" />info@lhv.ee
+        </h2>
+      </div>
+    </div>
+    <p class="footer-mobile-text">
+      Kui sul tekib pangateenuse kasutamisel probleeme, saad klienditoe
+      infotelefonilt abi ööpäev ringi.
+    </p>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { inject } from "vue";
+
+const isMobile = inject("isMobile");
+</script>
 
 <style scoped>
 .footer {
   display: flex;
+  flex-wrap: wrap;
   padding: 3.75rem 0;
   gap: 1.88rem;
 }
@@ -39,11 +64,11 @@
   gap: 1rem;
 }
 
-.section h2 {
+.footer-title {
   font-weight: 300;
 }
 
-.section span {
+.footer-title-bold {
   font-size: 1.38rem;
   line-height: 1.63rem;
   font-weight: 700;
@@ -80,5 +105,46 @@
   width: 1.5rem;
   height: 1.5rem;
   color: #3b3b47;
+}
+
+.footer-mobile {
+  display: flex;
+  flex-direction: column;
+  padding: 1.69rem;
+  padding-top: 0;
+  gap: 3rem;
+}
+
+.footer-mobile-text {
+  text-align: center;
+}
+
+.footer-mobile-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  align-items: center;
+}
+
+.footer-mobile-wrapper img {
+  width: 5rem;
+  height: 5rem;
+}
+
+.contact-mobile-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+.contact-mobile-wrapper img {
+  width: 1.5rem;
+  height: 1.5rem;
+  color: #3b3b47;
+}
+
+.contact-mobile-wrapper h2 {
+  display: flex;
+  gap: 1rem;
 }
 </style>
